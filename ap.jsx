dfebb9aa0851c4d33baa08f4276864cb54f05c0f
Shraddha3112-sih <shraddha2004shukla@gmail.com>
2026-03-26 10:30:08 +0530

    Add files via upload

---
+++ ap.jsx
@@ -0,0 +1,6 @@
+const About = () => (
+    <div className="p-10 bg-white rounded-xl shadow-sm text-center">
+      <h1 className="text-4xl font-bold text-green-600">📖 About Us</h1>
+      <p className="mt-4 text-gray-600">This section explains our mission and vision.</p>
+    </div>
+  );
