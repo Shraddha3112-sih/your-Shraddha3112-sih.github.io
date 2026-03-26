ba0f69d32dc6a67e9267ed1560cf952937174174
Shraddha3112-sih <shraddha2004shukla@gmail.com>
2026-03-26 10:30:53 +0530

    Add files via upload

---
+++ hp.jsx
@@ -0,0 +1,6 @@
+const Home = () => (
+    <div className="p-10 bg-white rounded-xl shadow-sm text-center">
+      <h1 className="text-4xl font-bold text-blue-600">🏠 Home Page</h1>
+      <p className="mt-4 text-gray-600">Welcome to the main landing area of our  site.</p>
+    </div>
+  );
